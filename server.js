c17c509faa1694687335dd07441b4de1b197e73d
Dao Nguyen <daon@us.ibm.com>
2017-04-14 13:51:38 -0400

    Test

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ var products = require("./products");
 app.get('/api/v1/products', function(req, res) {
   products.list(req,res);
 });
+app.get('/api/v1/welcome', function(req, res) {
+	res.send("Hello Sunil");
+});
 var port = process.env.PORT || 9080;
 app.listen(port, function() {
     console.log('App started on port:' + port);
